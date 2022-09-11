dbaf5a084a1c08e5288dcb0402ca3e9f817039f7
John <110991147+NjongeJ@users.noreply.github.com>
2022-09-11 03:23:36 -0700

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the phrase with proper grammar, but the outcome is a piece of art,
+ * main - prints the phrase with proper grammar,
  * Return: Always 0 (Success)
  */
 int main(void)
