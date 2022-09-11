e6b24029dfed3e765531e6664f4f05d8c926329a
John <110991147+NjongeJ@users.noreply.github.com>
2022-09-11 03:13:43 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio.h> 
- #include <unistd.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Return: Always 1 (Success) 
-  */ 
- int main(void) 
- { 
-         write(2, 
- "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
-         return (1); 
- }
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that place of art is useful",
+ * - Dora Korpar, 2015-10-19,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Entry point 
-  * 
-  * Description: C program that prints using printf 
-  * 
-  * Return: Always 0 (success) 
-  */ 
-  
- int main(void) 
- { 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
